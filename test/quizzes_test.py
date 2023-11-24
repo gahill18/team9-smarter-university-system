a78f1fa244afd8739463f3e0f41e41b2287ab456
Garrett Hill <gahill2018@pm.me>
2023-11-24 17:28:05 -0500

    intentionally broke discussions_test.json to show what malformed data could do to the load_data function, and created a test case for it

--- test/quizzes_test.py
+++ test/quizzes_test.py
@@ -29,6 +29,10 @@ class QuizzesTest(unittest.TestCase):
         function of the QuizzesController module. Adding "None" into the list
         of quizzes before trying to add another quiz will result in an
         AttributeError on line 54 of quizzes_controller.py
+
+        We assert that the id returned when adding a quiz object to the controller
+        should be the same id of the quiz object returned when querying the
+        get_quiz_by_id function for that ID
         """
         self.ctrl.quizzes.append(None)
         q_id = self.ctrl.add_quiz("Good Title", "Good Text", datetime(2001, 1, 24), datetime(2003, 7, 13))
@@ -36,15 +40,26 @@ class QuizzesTest(unittest.TestCase):
         self.assertEqual(
             quiz.id,
             q_id,
-            'The newly created quiz ID should match the ID of the queried quiz'
+            'The newly created quiz ID should match the ID of the queried quiz!'
         )
     
     def test_expose_failure_03(self):
         """
-        Implement this function and two more that
-        execute the code and make it fail.
+        This function demonstrates a lack of error checking in the _load_data
+        function of the QuizController module. Notice how using an integer
+        instead of a string for the title induces a TypeError on line 63 of
+        quizzes_controller.py
+
+        We assert that in the event of a failure to read data from a file,
+        None should be returned
         """
-        self.assertTrue(True, 'Example assertion.')
+        self.ctrl.file_name = 'discussions_test.json'
+        quizzes = self.ctrl._load_data()
+        self.assertEqual(
+            quizzes,
+            None,
+            'Quizzes should have failed to read from a poorly structured JSON!'
+        )
     
 if __name__ == '__main__':
     unittest.main()
